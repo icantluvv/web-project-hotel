a38bd763013a5cc7674b9505c4054d062814879a
Sergey <99052479+icantluvv@users.noreply.github.com>
2024-05-22 16:52:02 +0300

    add config for server pull (i know i must hide it)

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        baseApi: "http://127.0.0.1:7777/api/",
+        // baseApi: "http://127.0.0.1:7777/api/",
+        baseApi: "http://178.154.206.159:3030/api/",
         YANDEX_MAPS_API_KEY: "91dedc19-6d5d-468c-9715-0268910488da",
     },
     images: {
